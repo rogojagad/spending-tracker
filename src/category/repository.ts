570ef88fd1676d8b56b9fb98c88378dd7415bc52
rogojagad <rogojagadalit@gmail.com>
2024-09-22 22:04:19 +0000

    tech: return category in sorted priority

--- src/category/repository.ts
+++ src/category/repository.ts
@@ -3,11 +3,12 @@ import sql from "~/src/postgre.ts";
 export interface ICategory {
   id: string;
   name: string;
+  priority: number;
 }
 
 const getAll = async (): Promise<ICategory[]> => {
   const result = await sql<ICategory[]>`
-    SELECT * FROM category
+    SELECT * FROM category ORDER BY priority ASC
   `;
 
   return result;
