f159afd8d31fa09beda2914ba26c571db510e5a5
rogojagad <rogojagadalit@gmail.com>
2024-09-22 21:28:23 +0000

    try fix error on keep alive

--- src/cron.ts
+++ src/cron.ts
@@ -3,7 +3,7 @@ import ky from "ky";
 function register() {
   /** Just to keep the instance alive, so that not counted as "idle" and the instance is terminated */
   Deno.cron("Keep Alive", "* * * * *", async () => {
-    const result = await ky.get("0.0.0.0:80");
+    const result = await ky.get("https://127.0.0.1:80");
 
     console.log(`Keep alive result: ${await result.json()}`);
   });
