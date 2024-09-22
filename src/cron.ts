917365aa3fdcbf9f25905f04efb505a8e7954a47
rogojagad <rogojagadalit@gmail.com>
2024-09-22 21:15:36 +0000

    update keep alive logic

--- src/cron.ts
+++ src/cron.ts
@@ -2,5 +2,7 @@ import ky from "ky";
 
 /** Just to keep the instance alive, so that not counted as "idle" and the instance is terminated */
 Deno.cron("Keep Alive", "1 * * * *", async () => {
-  await ky.get("0.0.0.0:80");
+  const result = await ky.get("0.0.0.0:80");
+
+  console.log(`Keep alive result: ${await result.json()}`);
 });
