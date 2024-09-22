515d1b82465ce1f5d1dbefc8f8882f218d5b381c
rogojagad <rogojagadalit@gmail.com>
2024-09-22 21:24:17 +0000

    fix wrong cron

--- src/cron.ts
+++ src/cron.ts
@@ -2,7 +2,7 @@ import ky from "ky";
 
 function register() {
   /** Just to keep the instance alive, so that not counted as "idle" and the instance is terminated */
-  Deno.cron("Keep Alive", "1 * * * *", async () => {
+  Deno.cron("Keep Alive", "* * * * *", async () => {
     const result = await ky.get("0.0.0.0:80");
 
     console.log(`Keep alive result: ${await result.json()}`);
