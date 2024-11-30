300278047233c627745cdffa4dd3c02a1a195cb2
rogojagad <rogojagadalit@gmail.com>
2024-11-30 05:10:44 +0000

    chore: use conversations for telegram bot

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,23 +1,52 @@
-import { Bot } from "grammy";
-import TelegramBotHandler from "~/src/bot/handler.ts";
+import { Bot, Context, session } from "grammy";
+import {
+  type Conversation,
+  type ConversationFlavor,
+  ConversationFn,
+  conversations,
+  createConversation,
+} from "grammy/conversations";
+import { addNewSpending } from "~/src/bot/conversations/addNewSpending.ts";
 
+export type MyContext = Context & ConversationFlavor;
+export type MyConversation = Conversation<Context>;
+type BotClient = Bot<MyContext>;
+
+/**
+ * A wrapper to the Telegram bot client.
+ *
+ * This class is responsible for
+ *
+ * - initializing the instance
+ * - register all required middlewares and event listeners
+ * - sending message from app to user
+ */
 export default class TelegramBot {
-  private bot: Bot;
-  private handler: TelegramBotHandler;
+  private bot: BotClient;
   private recipientId: string;
+  private conversationHandlerRegistrar = new ConversationHandlerRegistrar();
 
-  constructor(handler: TelegramBotHandler) {
+  constructor() {
+    // init instance
     const botToken = Deno.env.get("TELEGRAM_BOT_TOKEN");
-
     if (!botToken?.length) throw new Error(`Bot token undefined`);
 
-    this.bot = new Bot(botToken);
-    this.handler = handler;
-    this.handler.registerHandler(this.bot);
+    const botClient = new Bot<MyContext>(botToken);
+
+    // register middlewares
+    botClient.use(session({ initial: () => ({}) }));
+    botClient.use(conversations());
+
+    // register conversations handler
+    this.conversationHandlerRegistrar.register(botClient);
+
+    // persists instance and other relevant values
+    // this should be final step, all event handler registration should be done before this
+    this.bot = botClient;
     this.recipientId = this.getRecipientId();
   }
 
-  getRecipientId(): string {
+  private getRecipientId(): string {
     const recipientId = Deno.env.get("TELEGRAM_RECIPIENT_ID");
 
     if (!recipientId?.length) throw new Error(`Telegram recipient ID unset`);
@@ -29,7 +58,7 @@ export default class TelegramBot {
     this.bot.start();
   }
 
-  getClientInstance(): Bot {
+  getClientInstance(): BotClient {
     return this.bot;
   }
 
@@ -42,5 +71,22 @@ export default class TelegramBot {
     console.info(
       `Success sending message | Message ID: ${messageInstance.message_id} | Text: ${message}`,
     );
+  }
+}
+
+class ConversationHandlerRegistrar {
+  register(clientInstance: BotClient): void {
+    this.registerAddNewSpendings(clientInstance);
+    // add other conversation handler
+  }
+
+  private registerAddNewSpendings(clientInstance: BotClient) {
+    clientInstance.use(
+      createConversation(addNewSpending as ConversationFn<Context>),
+    );
+
+    clientInstance.hears(/^(\d+)\s+(.+)$/, async (ctx) => {
+      await ctx.conversation.enter("addNewSpending");
+    });
   }
 }
