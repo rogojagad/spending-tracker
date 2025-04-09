573f8be42aa40d3caa4350890877d77dd5339a2a
rogojagad <rogojagadalit@gmail.com>
2025-04-09 16:38:19 +0700

    tech: change response format for monthly summaries

--- src/spending/service.ts
+++ src/spending/service.ts
@@ -1,8 +1,18 @@
 import spendingRepository, {
   ISpendingWithCategoryNameAndSourceName,
-  ITotalSpendingAmountByMonthAndCategoryName,
 } from "~/src/spending/repository.ts";
 import { IGetManySpendingsFilterQueryParam } from "~/src/spending/interface.ts";
+
+interface ISpendingAmountSummaryForMonth {
+  month: Date;
+  total: number;
+  summaries: IAmountForCategory[];
+}
+
+interface IAmountForCategory {
+  amount: number;
+  categoryName: string;
+}
 
 const getManySpendings = (
   filter: IGetManySpendingsFilterQueryParam,
@@ -13,10 +23,35 @@ const getManySpendings = (
     .getSpendingsByCategoryIdSourceIdAndCreatedAtDatetimeRange(filter);
 };
 
-const getMonthlySpendingSummaries = (): Promise<
-  ITotalSpendingAmountByMonthAndCategoryName[]
+const getMonthlySpendingSummaries = async (): Promise<
+  ISpendingAmountSummaryForMonth[]
 > => {
-  return spendingRepository.getSpendingSummariesGroupByMonthAndCategoryName();
+  const monthAndCategoryNameSpendings = await spendingRepository
+    .getSpendingSummariesGroupByMonthAndCategoryName();
+
+  const summariesByMonth = new Map<Date, IAmountForCategory[]>();
+
+  monthAndCategoryNameSpendings.forEach((spending) => {
+    const existingSummaryByMonth = summariesByMonth.get(spending.month) || [];
+
+    summariesByMonth.set(
+      spending.month,
+      existingSummaryByMonth.concat({
+        amount: spending.amount,
+        categoryName: spending.categoryName,
+      }),
+    );
+  });
+
+  return summariesByMonth.entries().map((entry) => {
+    const month = entry[0];
+    const summaries = entry[1];
+    const total = summaries.reduce((prev, next) => {
+      return prev + next.amount;
+    }, 0);
+
+    return { month, total, summaries };
+  }).toArray();
 };
 
 const spendingService = { getManySpendings, getMonthlySpendingSummaries };
