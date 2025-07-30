e276c275e2b7b0b2ae9dd0e3c7589dfd077cf931
rogojagad <rogojagadalit@gmail.com>
2025-07-30 20:10:30 +0700

    feat: add migration to create and configure application_period column in limit table

---
+++ db/migrations/20250730200614_create_limit_application_period.ts
@@ -0,0 +1,29 @@
+import {
+  AbstractMigration,
+  ClientPostgreSQL,
+  Info,
+} from "https://deno.land/x/nessie@2.0.11/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+  /** Runs on migrate */
+  async up(info: Info): Promise<void> {
+    // create application_period column
+    await this.client.queryArray(
+      `alter table limit add column application_period text;`,
+    );
+
+    // update application period to `MONTHLY` by default
+    await this.client.queryArray(
+      `update limit set application_period = 'MONTHLY'`,
+    );
+
+    // make application_period column non-nullable
+    await this.client.queryArray(
+      `alter table limit alter column application_period set not null;`,
+    );
+  }
+
+  /** Runs on rollback */
+  async down(info: Info): Promise<void> {
+  }
+}
