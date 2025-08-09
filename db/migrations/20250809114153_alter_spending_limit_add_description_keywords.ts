cdea26e78e808ab33460bab8e400f4cd27ceed33
rogojagad <rogojagadalit@gmail.com>
2025-08-09 19:49:06 +0700

    feat: add migration to alter spending_limit table by adding description_keywords column and updating constraints

---
+++ db/migrations/20250809114153_alter_spending_limit_add_description_keywords.ts
@@ -0,0 +1,45 @@
+import {
+  AbstractMigration,
+  ClientPostgreSQL,
+  Info,
+} from "https://deno.land/x/nessie@2.0.11/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+  /** Runs on migrate */
+  async up(info: Info): Promise<void> {
+    // create description_keywords column
+    await this.client.queryArray(
+      `ALTER TABLE spending_limit ADD COLUMN description_keywords TEXT[];`,
+    );
+
+    // drop the constraint that requires category_id or source_id to be not null
+    await this.client.queryArray(
+      `ALTER TABLE DROP CONSTRAINT limits_require_category_or_source_check`,
+    );
+
+    // ensure that either description_keywords, category_id, or source_id is not null
+    await this.client.queryArray(
+      `
+      ALTER TABLE ADD CONSTRAINT limits_require_category_or_source_check_or_description_keywords
+      CHECK (description_keywords IS NOT NULL  OR category_id IS NOT NULL OR source_id IS NOT NULL)
+      `,
+    );
+  }
+
+  /** Runs on rollback */
+  async down(info: Info): Promise<void> {
+    await this.client.queryArray(
+      `ALTER TABLE DROP CONSTRAINT limits_require_category_or_source_check_or_description_keywords;`,
+    );
+
+    await this.client.queryArray(
+      `ALTER TABLE CREATE CONSTRAINT limits_require_category_or_source_check CHECK (
+          category_id IS NOT NULL OR source_id IS NOT NULL
+        );`,
+    );
+
+    await this.client.queryArray(
+      `ALTER TABLE spending_limit DROP COLUMN description_keywords;`,
+    );
+  }
+}
