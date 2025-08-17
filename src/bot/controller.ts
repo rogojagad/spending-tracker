c2ed2428ae5bdf381ca3361b453a8e8efe8ba3fb
rogojagad <rogojagadalit@gmail.com>
2025-08-17 21:22:03 +0700

    fix: change messages endpoint from POST to GET to align with expected HTTP method

--- src/bot/controller.ts
+++ src/bot/controller.ts
@@ -8,7 +8,7 @@ const app = new Hono();
 app.use(logger());
 app.use(cors());
 
-app.post("/messages", (c: Context) => {
+app.get("/messages", (c: Context) => {
   console.info(JSON.stringify(c.req, null, 2));
   return c.json({ data: "OK" });
 });
